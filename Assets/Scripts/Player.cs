a57caac524487352a59913f8f8c31ea807b19593
MartinAllsbrook <martin.allsbrook@colorado.edu>
2026-01-07 10:22:38 -0500

    Clean up play script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,50 +4,17 @@ using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private GameObject defensePrefab;
-    [SerializeField] private GameObject defensePreviewPrefab;
     [SerializeField] private Tilemap hexTilemap;
     [SerializeField] private GameObject hexagonHighlightPrefab;
-    [SerializeField] private Enemy enemy;
-    [SerializeField] private RuleTile wallTile;
 
-    private bool placingDefense = false;
-    private GameObject defensePreviewInstance;
     private GameObject hexHighlightInstance;
     private Vector3Int lastHighlightedCell = new Vector3Int(int.MinValue, int.MinValue, 0);
-
-    public void CreateDefense()
-    {
-        placingDefense = true;
-        
-        // Create preview at cursor position
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        mouseWorldPos.z = 0f;
-        defensePreviewInstance = Instantiate(defensePreviewPrefab, mouseWorldPos, Quaternion.identity);
-    }
-
-    public void OnCancelPlacement()
-    {
-        placingDefense = false;
-        
-        // Clean up preview
-        if (defensePreviewInstance != null)
-        {
-            Destroy(defensePreviewInstance);
-        }
-    }
 
     private void Update()
     {
         // Get mouse position in world space
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         mouseWorldPos.z = 0f;
-        
-        // Update defense preview position to follow cursor
-        if (placingDefense && defensePreviewInstance != null)
-        {
-            defensePreviewInstance.transform.position = mouseWorldPos;
-        }
 
         // Highlight the hexagonal tile under the cursor
         if (hexTilemap != null && hexagonHighlightPrefab != null)
@@ -72,53 +39,6 @@ public class Player : MonoBehaviour
                 // Position the highlight at the center of the hexagonal tile
                 hexHighlightInstance.transform.position = cellCenterWorld;
             }
-        }
-    }
-
-    // Called by the new Input System when the Click action is triggered
-    public void OnClick(InputAction.CallbackContext context)
-    {
-        // Only respond when the button is pressed (not released)
-        if (context.performed && placingDefense)
-        {
-            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            mouseWorldPos.z = 0f;
-            Vector3Int cellPosition = hexTilemap.WorldToCell(mouseWorldPos);
-            hexTilemap.SetTile(cellPosition, wallTile); // Remove tile for test
-
-            
-            // Instantiate(defensePrefab, mouseWorldPos, Quaternion.identity);
-            
-            // // Clean up preview
-            // if (defensePreviewInstance != null)
-            // {
-            //     Destroy(defensePreviewInstance);
-            // }
-            
-            // placingDefense = false;
-        } 
-        else
-        {
-            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            mouseWorldPos.z = 0f;
-            Vector3Int cellPosition = hexTilemap.WorldToCell(mouseWorldPos);
-            enemy.MoveToLocation(new Vector2Int(cellPosition.x, cellPosition.y));
-        }
-    }
-
-    // Called by the new Input System when the Cancel action is triggered
-    public void OnCancel(InputAction.CallbackContext context)
-    {
-        // Only respond when the button is pressed (not released)
-        if (context.performed && placingDefense)
-        {
-            // Clean up preview
-            if (defensePreviewInstance != null)
-            {
-                Destroy(defensePreviewInstance);
-            }
-            
-            placingDefense = false;
         }
     }
 }
