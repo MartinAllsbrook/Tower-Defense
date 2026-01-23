af708ac7479abea49e51332bcfe176a5b56a106c
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2026-01-23 13:00:31 -0500

    Moved and renamed EnemyPathfinding to Pathfinding

--- Assets/Scripts/Generic/Pathfinding.cs
+++ Assets/Scripts/Generic/Pathfinding.cs
@@ -1,9 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 
-class EnemyPathfinding : MonoBehaviour
+class Pathfinding : MonoBehaviour
 {
     [SerializeField] bool usePathfindingManager = true; // Toggle to use centralized manager
     
